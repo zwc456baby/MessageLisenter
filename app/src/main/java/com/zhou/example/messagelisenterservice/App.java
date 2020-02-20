847649aaf93362230c4a280677082eaeb9f96cae
wbchenyuan <wbchenyuan@centaline.com.cn>
2020-02-20 20:54:56 +0800

    remove wanning

--- app/src/main/java/com/zhou/example/messagelisenterservice/App.java
+++ app/src/main/java/com/zhou/example/messagelisenterservice/App.java
@@ -4,8 +4,6 @@ import android.app.Application;
 import android.os.Process;
 import android.util.Log;
 
-import com.zhou.netlogutil.NetLogUtil;
-
 public class App extends Application {
     @Override
     public void onCreate() {
