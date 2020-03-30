e1095907fe1f9396a997917522971ed1a9f93312
zhouwenchao <zhouwenchao@vilgame.com>
2020-03-30 11:27:00 +0800

    支持创建快捷方式

--- app/src/main/java/com/zhou/example/messagelisenterservice/SettingActivity.java
+++ app/src/main/java/com/zhou/example/messagelisenterservice/SettingActivity.java
@@ -1,12 +1,17 @@
 package com.zhou.example.messagelisenterservice;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ShortcutInfo;
+import android.content.pm.ShortcutManager;
+import android.graphics.drawable.Icon;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.provider.Settings;
@@ -20,7 +25,7 @@ public class SettingActivity extends Activity {
 
     private EditText appNameEdit, titleInEdit, messageInEdit, playSleepTime, pauseNotifyEdit, netLogUrlEdit, accountEdit, filenameEdit;
 
-    private Button startBtn, startSettingBtn;
+    private Button startBtn, startSettingBtn, createShortcut;
 
     private CheckBox playMusic, zhengDong, containsService, pauseNotifyCheck, pauseApplyToAllPageCheck;
 
@@ -46,6 +51,7 @@ public class SettingActivity extends Activity {
 
         startBtn = findViewById(R.id.startBtn);
         startSettingBtn = findViewById(R.id.startSettingBtn);
+        createShortcut = findViewById(R.id.createShortcut);
 
         playMusic = findViewById(R.id.playMusicCheck);
         zhengDong = findViewById(R.id.zhendongCheck);
@@ -86,6 +92,17 @@ public class SettingActivity extends Activity {
             @Override
             public void onClick(View view) {
                 gotoNotificationAccessSetting(SettingActivity.this);
+            }
+        });
+        createShortcut.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    createShortcut2();
+                } else {
+                    createShortcut();
+                }
             }
         });
     }
@@ -155,6 +172,46 @@ public class SettingActivity extends Activity {
                 e1.printStackTrace();
             }
             e.printStackTrace();
+        }
+    }
+
+    private void createShortcut() {
+        Intent shortcutIntent = new Intent();
+        shortcutIntent.setComponent(new ComponentName(this, SettingActivity.class));
+        //给Intent添加 对应的flag
+        shortcutIntent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS | Intent.FLAG_ACTIVITY_NEW_TASK);
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,
+                Intent.ShortcutIconResource.fromContext(this,
+                        R.drawable.ic_launcher));
+        resultIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+
+        //这里可以设置快捷方式的名称
+        resultIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, getString(R.string.history_list));
+        //设置Action
+        resultIntent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
+        //发送广播、通知系统创建桌面快捷方式
+        sendBroadcast(resultIntent);
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    private void createShortcut2() {
+        ShortcutManager shortcutManager = (ShortcutManager) getSystemService(Context.SHORTCUT_SERVICE);
+        if (shortcutManager == null) return;
+        if (shortcutManager.isRequestPinShortcutSupported()) {
+            Intent shortcutInfoIntent = new Intent(this, SettingActivity.class);
+            shortcutInfoIntent.setAction(Intent.ACTION_VIEW); //action必须设置，不然报错
+
+            ShortcutInfo info = new ShortcutInfo.Builder(this, getString(R.string.history_list))
+                    .setIcon(Icon.createWithResource(this, R.drawable.ic_launcher))
+                    .setShortLabel(getString(R.string.history_list))
+                    .setIntent(shortcutInfoIntent)
+                    .build();
+
+            //当添加快捷方式的确认弹框弹出来时，将被回调
+//            PendingIntent shortcutCallbackIntent = PendingIntent.getBroadcast(context, 0, new Intent(context, MyReceiver.class), PendingIntent.FLAG_UPDATE_CURRENT);
+
+            shortcutManager.requestPinShortcut(info, null);
         }
     }
 
